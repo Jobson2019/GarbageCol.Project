8007a00e73cda7e0ea02fe2d792ad182ec783d3d
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-29 10:06:41 -0500

    first commit

---
+++ GarbageCollectProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarbageCollectProject.Startup))]
+namespace GarbageCollectProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
