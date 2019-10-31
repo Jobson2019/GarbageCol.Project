c16981aa5be6ef5b8081632651e105d5bf720bdd
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-31 11:16:06 -0500

    Usercontroller completed and implemented

--- GarbageCollectProject/Controllers/AccountController.cs
+++ GarbageCollectProject/Controllers/AccountController.cs
@@ -174,11 +174,7 @@ namespace GarbageCollectProject.Controllers
                     {
                         return RedirectToAction("Create", "Employee");
                     }
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                   
                 }
                 AddErrors(result);
             }
---
+++ GarbageCollectProject/Controllers/userController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GarbageCollectProject.Controllers
+{   
+    [Authorize]
+    public class userController : Controller
+    {
+        // GET: user
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
