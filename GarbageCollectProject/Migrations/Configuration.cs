d0ff543cd3d91a055602172d7e3c7c5dc1a3d15a
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-31 08:35:01 -0500

    automatic migrations enabled

--- GarbageCollectProject/Migrations/Configuration.cs
+++ GarbageCollectProject/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace GarbageCollectProject.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(GarbageCollectProject.Models.ApplicationDbContext context)
