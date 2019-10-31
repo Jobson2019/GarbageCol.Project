2c5ca7f5b615bd9d92707ae5cd1fa8b41b9111a8
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-31 11:49:48 -0500

    user controller actually functional

--- GarbageCollectProject/Controllers/userController.cs
+++ GarbageCollectProject/Controllers/userController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GarbageCollectProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,7 +15,46 @@ namespace GarbageCollectProject.Controllers
         // GET: user
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isEmployeeUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
             return View();
         }
+
+        public Boolean isEmployeeUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Employee")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+
     }
 }
